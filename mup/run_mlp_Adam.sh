64c9998db5deff559c5ab5313d99f0673e6b5d71
Faraz <faraz@dynaroboics.ai>
2025-06-01 22:26:20 +0000

    june1

--- mup/run_mlp_Adam.sh
+++ mup/run_mlp_Adam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Running MLP Adam experiment..."
-python3 train_mlp.py --model SPMLP --subset 1 --optimizer Adam --lr_range -16 -4
+python3 train_mlp.py --model SPMLP --subset 1 --optimizer Adam --lr_range -16 -4 --lr_points 10
 
 echo "Running muMLP Adam experiment..."
-python3 train_mlp.py --model muMLP --subset 1 --optimizer Adam --lr_range -12 0
+python3 train_mlp.py --model muMLP --subset 1 --optimizer Adam --lr_range -12 0 --lr_points 10
 
 echo "All experiments completed."
--- mup/run_mlp_SGD.sh
+++ mup/run_mlp_SGD.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# echo "Running MLP Adam experiment..."
-# python3 train_mlp.py --model SPMLP --subset 1 --optimizer Adam --lr_range -16 -4
-
-# echo "Running muMLP Adam experiment..."
-# python3 train_mlp.py --model muMLP --subset 1 --optimizer Adam --lr_range -12 0
-
 echo "Running MLP SGD experiment..."
 python3 train_mlp.py --model SPMLP --subset 1 --optimizer SGD --lr_range -12 0 --lr_points 10
 
